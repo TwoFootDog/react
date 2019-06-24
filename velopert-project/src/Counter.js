c92dafd52e5ebe2d105fce9b15f54434694e66bb
two dog <ekfrl2815@gmail.com>
2019-06-25 01:24:29 +0900

    aaa

--- velopert-project/src/Counter.js
+++ velopert-project/src/Counter.js
@@ -1,4 +1,11 @@
-import React, {Component} from 'react'
+import React, {Component} from 'react';
+
+const Problematic = () => {
+    throw (new Error('버그가 나타났다!'));
+    return (
+        <div></div>
+    );
+};
 
 class Counter extends Component {
     state = {
@@ -6,11 +13,12 @@ class Counter extends Component {
         count: {
             plusCount: 0,
             minusCount: 0
-        }
+        },
+        error: false
     }
     constructor(props) {
-        super(props)
-        console.log("constructor")
+        super(props);
+        console.log('constructor');
     }
     // constructor(props) {
     //     super(props)
@@ -19,6 +27,42 @@ class Counter extends Component {
     //     }
     // }
 
+    componentWillMount() {
+        // 컴포넌트가 화면에 나가기 직전에 호출됨
+        console.log('componentWillMount (deprecated)');
+    }
+
+    componentDidMount() {
+        // 컴포넌트가 화면에 나타나게 됐을 때 호출됨
+        console.log('componentDidMount');
+    }
+
+    shouldComponentUpdate(nextProps, nextState) {
+        // 5의 배수면 리렌더링하지 않음
+        console.log('shouldComponentUpdate');
+        if (nextState.number % 5 === 0) {
+            return false;
+        }
+        return true;
+    }
+
+    componentWillUpdate(nextProps, nextState) {
+        // shouldComponentUpdate가 true를 반환했을 때만 호출됨
+        console.log('componentWillUpdate');
+    }
+
+    componentDidUpdate(prevProps, prevState) {
+        // render()를 호출하고 난 다음에 발생
+        console.log('componentDidUpdate');
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({
+            error: true
+        })
+    }
+
+    // 비구조화 할당
     handleIncrease = () => {
         this.setState(
             ({number, count}) => ({
@@ -56,10 +100,13 @@ class Counter extends Component {
     }
     
     render() {
+        if (this.state.error) return (<h1>에러 발생!{this.state.number}</h1>)
+        console.log('render');
         return (
             <div>
                 <h1>카운터</h1>
                 <div>값 : {this.state.number}</div>
+                {this.state.number === 4 && <Problematic />}
                 <div>plusCount: {this.state.count.plusCount}</div>
                 <div>minusCount: {this.state.count.minusCount}</div>
                 <button onClick={this.handleIncrease}>+</button>
