738b86aecd49e66655640db52616cb83291afb59
two dog <ekfrl2815@gmail.com>
2019-07-06 02:43:36 +0900

    aa

--- api-call-test/src/Common/RestApi.js
+++ api-call-test/src/Common/RestApi.js
@@ -3,9 +3,15 @@ import axios from 'axios';
 
 const ApiURL = "http://198.13.47.188:8080/batch";
 
+const TestURL = "http://127.0.0.1:8080/member/register"
+
 // 배치 전체 리스트 호출 API
 export const getBatchList = () => {
     return axios.get(ApiURL);
+}
+
+export const registMember = (data) => {
+    return axios.post(TestURL, {data}, {headers: {'Content-Type': 'application/json'}})
 }
 
 
--- api-call-test/src/Layout/SignUp.js
+++ api-call-test/src/Layout/SignUp.js
@@ -10,8 +10,11 @@ import Grid from '@material-ui/core/Grid';
 import Box from '@material-ui/core/Box';
 import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
 import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import Container from '@material-ui/core/Container';
+import * as RESTAPI from '../Common/RestApi';
+import axios from 'axios';
 
 // function MadeWithLove() {
 //   return (
@@ -25,7 +28,7 @@ import Container from '@material-ui/core/Container';
 //   );
 // }
 
-const useStyles = makeStyles(theme => ({
+const useStyles = theme => ({
   '@global': {
     body: {
       backgroundColor: theme.palette.common.white,
@@ -48,108 +51,185 @@ const useStyles = makeStyles(theme => ({
   submit: {
     margin: theme.spacing(3, 0, 2),
   },
-}));
+});
 
-export default function SignUp() {
-  const classes = useStyles();
 
-  return (
-    <Container component="main" maxWidth="xs">
-      <CssBaseline />
-      <div className={classes.paper}>
-        <Avatar className={classes.avatar}>
-          <LockOutlinedIcon />
-        </Avatar>
-        <Typography component="h1" variant="h5">
-          Sign up
-        </Typography>
-        <form className={classes.form} noValidate>
-          <Grid container spacing={2}>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                autoComplete="fname"
-                name="firstName"
-                variant="filled"
-                required
-                fullWidth
-                id="firstName"
-                label="First Name"
-                autoFocus
-              />
+class SignUp extends React.Component {
+  state = {
+    userId: null,
+    userPasswd: null,
+    userEmail: null,
+    userFirstName: null,
+    userLastName: null,
+  }
+
+  constructor(props) {
+    super(props);
+  }
+
+  postSend = async () => {
+    // const {userId, userPasswd, userEmail, userFirstName, userLastName} = this.state;
+    const user = {
+      userId: this.state.userId,
+      userPasswd: this.state.userPasswd,
+      userEmail: this.state.userEmail,
+      userFirstName: this.state.userFirstName,
+      userLastName: this.state.userLastName,
+    }
+    try {
+      // await RESTAPI.registMember(user);
+      axios.post('http://127.0.0.1:8080/member/register', {user}, {headers: {'Content-Type': 'application/json'}})
+    } catch(err) {
+      console.log(err);
+    }
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault(); // 페이지 리로딩 방지
+    // post function 호출
+    // this.postSend();
+    const user = {
+      userId: this.state.userId,
+      userPasswd: this.state.userPasswd,
+      userEmail: this.state.userEmail,
+      userFirstName: this.state.userFirstName,
+      userLastName: this.state.userLastName,
+    }
+    try {
+      console.log(user);
+      // await RESTAPI.registMember(user);
+      axios.post('http://127.0.0.1:8080/member/register', {user}, {headers: {'Content-Type': 'application/json'}})
+    } catch(err) {
+      console.log(err);
+    }
+
+    // this.setState({
+    //   userId: null,
+    //   userPasswd: null,
+    //   userEmail: null,
+    //   userFirstName: null,
+    //   userLastName: null,
+    // })
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value
+    })
+  }
+  render() {
+    const {classes} = this.props;
+
+    return (
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign up
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={this.handleSubmit}>
+            <Grid container spacing={2}>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  autoComplete="fname"
+                  name="userFirstName"
+                  variant="filled"
+                  required
+                  fullWidth
+                  id="userFirstName"
+                  label="First Name"
+                  value={this.userFirstName}
+                  onChange={this.handleChange}
+                  autoFocus
+                />
+              </Grid>
+              <Grid item xs={12} sm={6}>
+                <TextField
+                  variant="filled"
+                  required
+                  fullWidth
+                  id="userLastName"
+                  label="Last Name"
+                  name="userLastName"
+                  autoComplete="lname"
+                  value={this.userLastName}
+                  onChange={this.handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="filled"
+                  required
+                  fullWidth
+                  id="userEmail"
+                  label="Email Address"
+                  name="userEmail"
+                  autoComplete="email"
+                  value={this.userEmail}
+                  onChange={this.handleChange}
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="filled"
+                  required
+                  fullWidth
+                  id="userId"
+                  label="User ID"
+                  name="userId"
+                  value={this.userId}
+                  onChange={this.handleChange}
+                  // autoComplete="email"
+                />
+              </Grid>
+              <Grid item xs={12}>
+                <TextField
+                  variant="filled"
+                  required
+                  fullWidth
+                  name="userPasswd"
+                  label="Password"
+                  type="password"
+                  id="userPasswd"
+                  autoComplete="current-password"
+                  value={this.userPasswd}
+                  onChange={this.handleChange}
+                />
+              </Grid>
+              {/* <Grid item xs={12}>
+                <FormControlLabel
+                  control={<Checkbox value="allowExtraEmails" color="primary" />}
+                  label="I want to receive inspiration, marketing promotions and updates via email."
+                />
+              </Grid> */}
             </Grid>
-            <Grid item xs={12} sm={6}>
-              <TextField
-                variant="filled"
-                required
-                fullWidth
-                id="lastName"
-                label="Last Name"
-                name="lastName"
-                autoComplete="lname"
-              />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+            <Grid container justify="flex-end">
+              <Grid item xs>
+                <Link href="/SignIn" variant="body2">
+                  Already have an account? Sign in
+                </Link>
+              </Grid>
             </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="filled"
-                required
-                fullWidth
-                id="email"
-                label="Email Address"
-                name="email"
-                autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="filled"
-                required
-                fullWidth
-                id="userId"
-                label="User ID"
-                name="userId"
-                // autoComplete="email"
-              />
-            </Grid>
-            <Grid item xs={12}>
-              <TextField
-                variant="filled"
-                required
-                fullWidth
-                name="password"
-                label="Password"
-                type="password"
-                id="password"
-                autoComplete="current-password"
-              />
-            </Grid>
-            {/* <Grid item xs={12}>
-              <FormControlLabel
-                control={<Checkbox value="allowExtraEmails" color="primary" />}
-                label="I want to receive inspiration, marketing promotions and updates via email."
-              />
-            </Grid> */}
-          </Grid>
-          <Button
-            type="submit"
-            fullWidth
-            variant="contained"
-            color="primary"
-            className={classes.submit}
-          >
-            Sign Up
-          </Button>
-          <Grid container justify="flex-end">
-            <Grid item xs>
-              <Link href="/SignIn" variant="body2">
-                Already have an account? Sign in
-              </Link>
-            </Grid>
-          </Grid>
-        </form>
-      </div>
-      {/* <Box mt={5}>
-        <MadeWithLove />
-      </Box> */}
-    </Container>
-  );
+          </form>
+        </div>
+        {/* <Box mt={5}>
+          <MadeWithLove />
+        </Box> */}
+      </Container>
+    );
+  }
+  
 }
+export default withStyles(useStyles)(SignUp);
